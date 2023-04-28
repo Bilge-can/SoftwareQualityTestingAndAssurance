5a14223942048d77890eb9a706dde6740ad5d19e
Bilge-can <bilgecan618@gmail.com>
2023-04-28 14:24:39 +0300

    Test

---
+++ PerformanceTest/PerformanceTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace perfomans1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 1; i<=100; i++)
+            {
+                toplam = toplam + i;
+
+            }
+            Console.WriteLine("1den 100e kadar olan tam sayıların toplamı={0}, toplam");
+            stopwatch.Stop();
+
+            Console.WriteLine("for dongüsü gecen süre: " + stopwatch.Elapsed);
+    
+
+        }
+    }
+}
